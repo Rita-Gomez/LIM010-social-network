521a725962eabb95c172b207a29766c67e1349c1
andreamoscoso <ams_t2@hotmail.com>
2019-08-12 08:37:42 -0500

    subiendo estilos en html

---
+++ src/control.js
@@ -0,0 +1,5 @@
+// aqui exportaras las funciones que necesites
+// eslint-disable-next-line arrow-body-style
+export const verPersonaje = (x) => {
+  return x;
+};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
 // Este es el punto de entrada de tu aplicacion
-import { myFunction } from './lib/index.js';
+// import { verPersonaje } from './lib/index.js';
 
-myFunction();
+// eslint-disable-next-line no-console
+// console.log(verPersonaje('hOLA MUNDO'));
+
+// variables globales
+const save = document.getElementById('boton-guardar');
+const give = document.getElementById('boton-cargar');
+const login = document.getElementById('login');
+const page2 = document.getElementById('page2');
+const img = document.getElementById('img');
+// funcion
+save.addEventListener('click', () => {
+  /* Captura de datos escrito en los inputs */
+  const email = document.getElementById('emailtxt').value;
+  const contraseña = document.getElementById('contraseñatxt').value;
+  /* Guardando los datos en el LocalStorage */
+  localStorage.setItem('Email', email);
+  localStorage.setItem('Contraseña', contraseña);
+  /* Limpiando los campos o inputs */
+  document.getElementById('emailtxt').value = '';
+  document.getElementById('contraseñatxt').value = '';
+  img.classList.add('hide');
+  page2.classList.remove('hide');
+  login.classList.add('hide');
+});
+
+give.addEventListener('click', () => {
+  /* Obtener datos almacenados */
+  const email = localStorage.getItem('Email');
+  const contraseña = localStorage.getItem('Contraseña');
+  /* Mostrar datos almacenados */
+  document.getElementById('email').innerHTML = email;
+  document.getElementById('contraseña').innerHTML = contraseña;
+  login.classList.add('hide');
+});
