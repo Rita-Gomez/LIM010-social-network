c98b9ec3e0ca4b652d06e7f7dfdaf86b7eb3b659
Rtaperuvian <ritagomezh@gmail.com>
2019-08-15 18:33:15 -0500

    cambios de funcionabilidad de registro y alerts

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@
 // console.log(verPersonaje('hOLA MUNDO'));
 
 // variables globales
-const save = document.getElementById('boton-guardar');
-const give = document.getElementById('boton-segunda');
+const registrar = document.getElementById('boton-guardar');
+const ingresar = document.getElementById('boton-segunda');
 const volver = document.getElementById('boton-regresar');
 const login = document.getElementById('login');
 const ir = document.getElementById('ir-guardar');
@@ -15,36 +15,66 @@ const page2 = document.getElementById('page2');
 const respuesta = document.getElementById('respuesta');
 const invalida = document.getElementById('passinvalida');
 // funcion
-save.addEventListener('click', () => {
+registrar.addEventListener('click', () => {
   const email = document.getElementById('emailtxt').value;
   const nombre = document.getElementById('nametxt').value;
   const contraseña = document.getElementById('contraseñatxt').value;
-  localStorage.setItem('Nombre', nombre);
-  localStorage.setItem('Email', email);
-  localStorage.setItem('Contraseña', contraseña);
-  if (contraseña.length >= 6) {
-    invalida.innerHTML = '';
-  } else {
-    invalida.innerHTML = '<i class="bx bx-error-circle"></i> La contraseña debe tener al menos 6 caracteres';
-  }
+  const expresion = /\w+@\w+\.+[a-z]/; 
+  
+
+  // const arr = [];
+
+  // arr.push({nombre:'nuevo',  email:'usuario@hotmail.com',edad:20});
+  // arr.push({nombre:'usuario2', email:'usuario@hotmail.com', edad:1000})
+  // console.log(arr);
+ 
+  // localStorage.setItem('usuarios',  JSON.stringify(arr));
+ 
+  console.log(JSON.parse(localStorage.getItem('usuarios')));
+
+  // localStorage.setItem('Nombre', nombre);
+  // localStorage.setItem('Email', email);
+  // localStorage.setItem('Contraseña', contraseña);
+  if (email === ''|| nombre===''||contraseña==='') {
+    invalida.innerHTML = '<i class="bx bx-error-circle"></i> Todos los campos son obligatorios';
+    return false ;
+  
+} else if (contraseña.length<=6){
+ invalida.innerHTML =' <i class="bx bx-error-circle"></i> Contraseña mínimo de 6 caracteres';
+return false   ;
+}else if (!expresion.test(email)){
+  invalida.innerHTML='<i class="bx bx-error-circle"></i> Ingrese un correo válido';
+return false; 
+} else if (!isNaN(nombre)){
+  invalida.innerHTML =' <i class="bx bx-error-circle"></i> Por favor su nombre debe contener solo letras';
+  return false   ; 
+}else{
+  const probandoArr = [];
+  probandoArr.push ({nombre: nombre, email: email, contraseña: contraseña});
+localStorage.setItem('usuarios' , JSON.stringify(probandoArr));
+  invalida.innerHTML = 'Ya estas registrada(o)'
   document.getElementById('nametxt').value = '';
   document.getElementById('emailtxt').value = '';
   document.getElementById('contraseñatxt').value = '';
   page2.classList.remove('hide');
   login.classList.add('hide');
   volver.classList.remove('hide');
+  return true ;
+}
 });
 
-give.addEventListener('click', () => {
+
+ingresar.addEventListener('click', () => {
   const email = document.getElementById('email').value;
   const contraseña = document.getElementById('contraseña').value;
-  if (email === localStorage.getItem('Email') && contraseña === localStorage.getItem('Contraseña')) {
+  console.log(JSON.parse(localStorage.getItem('usuarios')));
+  if (email === localStorage.getItem('email') && contraseña === localStorage.getItem('contraseña')){
     /* respuesta.innerHTML = 'puedes ingresar'; */
     // eslint-disable-next-line no-alert
     alert('puedes ingresar');
   } else {
     respuesta.classList.remove('hide');
-    respuesta.innerHTML = '<i class="bx bx-error"></i>Lo sentimos. Has introducido una dirección o contraseña incorrecta.';
+    respuesta.innerHTML = '<i class="bx bx-error"></i>Lo sentimos. Has introducido una dirección electrónica o contraseña incorrecta.';
   }
 });
 
@@ -63,3 +93,5 @@ ir.addEventListener('click', () => {
   page2.classList.remove('hide');
   login.classList.add('hide');
 }); 
+
+
