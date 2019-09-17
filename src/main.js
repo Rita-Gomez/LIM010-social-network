c634120315ef9cfd245032b4d3a1c960d333d7f6
Rtaperuvian <ritagomezh@gmail.com>
2019-09-17 18:58:49 -0500

    testeo completo

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-alert */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-undef */
+// variables globales
 const btnRegistro = document.getElementById('btn-guardar');
 const btnRetornar = document.getElementById('btn-regresar');
 const btnIngresar = document.getElementById('btn-ingresar');
 const btnPublicar = document.getElementById('btn-mostrartexto');
 const enlaceRegistro = document.getElementById('ir-guardar');
 const btnSalir = document.getElementById('logout');
-const cajaImagen = document.getElementById('img');
-const cajaIngreso = document.getElementById('login');
-const cajaRegistro = document.getElementById('page2');
-const cajaMenu = document.getElementById('page3');
-const cajaPost = document.getElementById('page4');
+const inputImagen = document.getElementById('img');
+const inputIngreso = document.getElementById('login');
+const inputRegistro = document.getElementById('page2');
+const inputMenu = document.getElementById('page3');
+const inputPost = document.getElementById('page4');
 const msjErroneoLogin = document.getElementById('respuesta');
 const msjErroneoRegistro = document.getElementById('passalert');
 const msjErroneoPost = document.getElementById('dontlook');
@@ -18,33 +24,31 @@ const arrayLocalStorage = localStorage.getItem('usuarios') ? JSON.parse(localSto
 btnRegistro.addEventListener('click', () => {
   const email = document.getElementById('emailtxt').value;
   const nombre = document.getElementById('nametxt').value;
-  const contraseña = document.getElementById('contraseñatxt').value; 
-  const expresion = /\w+@\w+\.+[a-z]/;
+  const contraseña = document.getElementById('contraseñatxt').value;
   if (email === '' || nombre === '' || contraseña === '') {
     msjErroneoRegistro.innerHTML = '<i class="bx bx-error-circle"></i> Todos los campos son obligatorios';
     return false;
-  } else if (!isNaN(nombre)) {
+  } if (!data.validarNombre(nombre)) {
     msjErroneoRegistro.innerHTML = ' <i class="bx bx-error-circle"></i> Corregir nombre';
     return false;
-  } else if (contraseña.length < 6) {
+  } if (!data.validarPassword(contraseña)) {
     msjErroneoRegistro.innerHTML = ' <i class="bx bx-error-circle"></i> Debe ser como minimo 6 caracteres';
     return false;
-  } else if (!expresion.test(email)) {
+  } if (!data.validarEmail(email)) {
     msjErroneoRegistro.innerHTML = ' <i class="bx bx-error-circle"></i> Email invalido';
     return false;
-  } else {
-    const encriptar = window.cipher.encode(33, contraseña);
-    arrayLocalStorage.push({ nombre, email, encriptar });
-    localStorage.setItem('usuarios', JSON.stringify(arrayLocalStorage));
-    msjErroneoRegistro.innerHTML = 'Ya estas registrada(o)';
-    document.getElementById('nametxt').value = '';
-    document.getElementById('emailtxt').value = '';
-    document.getElementById('contraseñatxt').value = '';
-    cajaRegistro.classList.remove('hide');
-    cajaIngreso.classList.add('hide');
-    btnRetornar.classList.remove('hide');
-    return true;
   }
+  const encriptar = window.cipher.encode(33, contraseña);
+  arrayLocalStorage.push({ nombre, email, encriptar });
+  localStorage.setItem('usuarios', JSON.stringify(arrayLocalStorage));
+  msjErroneoRegistro.innerHTML = 'Ya estas registrada(o)';
+  document.getElementById('nametxt').value = '';
+  document.getElementById('emailtxt').value = '';
+  document.getElementById('contraseñatxt').value = '';
+  inputRegistro.classList.remove('hide');
+  inputIngreso.classList.add('hide');
+  btnRetornar.classList.remove('hide');
+  return true;
 });
 
 btnIngresar.addEventListener('click', () => {
@@ -53,15 +57,15 @@ btnIngresar.addEventListener('click', () => {
   const infoname = document.getElementById('ver-name');
   const parrafoname = document.getElementById('parrafonombre');
   for (let i = 0; i < arrayLocalStorage.length; i++) {
-    const descencriptar = window.cipher.decode(33, arrayLocalStorage[i].encriptar);    
+    const descencriptar = window.cipher.decode(33, arrayLocalStorage[i].encriptar);
     if (email === arrayLocalStorage[i].email && descencriptar === contraseñainput) {
-      cajaMenu.classList.remove('hide');
-      cajaPost.classList.remove('hide');
-      cajaIngreso.classList.add('hide');
-      cajaImagen.classList.add('hide');
+      inputMenu.classList.remove('hide');
+      inputPost.classList.remove('hide');
+      inputIngreso.classList.add('hide');
+      inputImagen.classList.add('hide');
       infoname.innerHTML = `Mi perfil: ${arrayLocalStorage[i].nombre}`;
       parrafoname.innerHTML = `${arrayLocalStorage[i].nombre}`;
-      pintarPost(getTextos());
+      pintarPost(data.leerLocalStorage('textos'));
     } else {
       msjErroneoLogin.classList.remove('hide');
       msjErroneoLogin.innerHTML = '<i class="bx bx-error"></i>Lo sentimos. Has introducido una dirección o contraseña incorrecta.';
@@ -69,18 +73,19 @@ btnIngresar.addEventListener('click', () => {
   }
 });
 
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', () => {  
-  const tipo = document.getElementById("contraseña");
-  if(tipo.type == "password"){
-      tipo.type = "text";
-      btn.innerHTML = `<i class='bx bxs-hide'></i>`;
-  }else{
-      tipo.type = "password";      
-      btn.innerHTML = `<i class='bx bx-show-alt'></i>`;
+const btnContraseña = document.querySelector('#btn');
+btnContraseña.addEventListener('click', () => {
+  const tipo = document.getElementById('contraseña');
+  if (tipo.type === 'password') {
+    tipo.type = 'text';
+    btnContraseña.innerHTML = '<i class=\'bx bxs-hide\'></i>';
+  } else {
+    tipo.type = 'password';
+    btnContraseña.innerHTML = '<i class=\'bx bx-show-alt\'></i>';
   }
 });
 
+// eslint-disable-next-line consistent-return
 btnPublicar.addEventListener('click', () => {
   const area = document.getElementById('textarea').value;
   if (area === '') {
@@ -88,29 +93,21 @@ btnPublicar.addEventListener('click', () => {
     msjVacioPost.classList.remove('hide');
     msjVacioPost.innerHTML = '<i class="bx bx-error-circle"></i> No has ingresado ningun texto';
     return false;
-  } else {
-    msjErroneoPost.classList.add('hide');
-    msjVacioPost.classList.add('hide');
-    document.getElementById('vertabla').classList.remove('hide');
-    document.getElementById('textarea').value = '';
-    const location1 = getTextos();
-    location1.push({ area });
-    localStorage.setItem('textos', JSON.stringify(location1));
-    console.log(location1);
-    pintarPost();
   }
+  msjErroneoPost.classList.add('hide');
+  msjVacioPost.classList.add('hide');
+  document.getElementById('vertabla').classList.remove('hide');
+  document.getElementById('textarea').value = '';
+  const location1 = data.leerLocalStorage('textos');
+  data.agregarElementoArray({ area }, location1);
+  data.guardarLocalStorage('textos', location1);
+  pintarPost(location1);
 });
 
- const getTextos =()=>{
-  return localStorage.getItem('textos') ? JSON.parse(localStorage.getItem('textos')) : [];
- };
-
-
-const pintarPost = () => {
+const pintarPost = (arr) => {
   const tabla = document.getElementById('tblDatos');
-  const arrayPostLocalStorage = getTextos();
   tabla.innerHTML = '';
-  arrayPostLocalStorage.forEach((obj, index) => {
+  arr.forEach((obj, index) => {
     const tr = document.createElement('tr');
     tr.innerHTML += `
 
@@ -123,28 +120,27 @@ const pintarPost = () => {
     tr.setAttribute('id', index);
     tabla.appendChild(tr);
     const btnEditar = tr.querySelector('.btnEditar');
-      btnEditar.addEventListener('click', () => {        
-        modificar(index, obj);
-      });
+    btnEditar.addEventListener('click', () => {
+      modificar(index, obj);
+    });
     const btnEliminar = tr.querySelector('.btnEliminar');
     btnEliminar.addEventListener('click', () => {
-        eliminar(index);
+      eliminar(index);
     });
   });
 };
 
 const modificar = (index, dataPost) => {
   const idtr = document.getElementById(index);
-  console.log('estoy consoleando my getelementbyid', idtr)
   idtr.innerHTML = ` 
   <td><input id="newText" placeholder="${dataPost.area}"></input></td>
   <td>
     <button class="btnModificar" data-index=${index}><!--<i class='bx bx-edit-alt'></i>--></button>
     <button class="btnCancel" data-index=${index}><!--<i class='bx bxs-calendar-x'></i>--></button>
-  </td>  `
+  </td>  `;
   const btnCancel = idtr.querySelector('.btnCancel');
-        btnCancel.addEventListener('click', () => {
-          idtr.innerHTML = `<td id="objTexto">${dataPost.area}</td>
+  btnCancel.addEventListener('click', () => {
+    idtr.innerHTML = `<td id="objTexto">${dataPost.area}</td>
         <td>
           <button class="btnEditar"><!--<i class='bx bxs-pencil'></i>--></button>
           <button class="btnEliminar"><!--<i class='bx bxs-trash'></i>--></button>
@@ -152,34 +148,33 @@ const modificar = (index, dataPost) => {
   });
   const btnModificar = idtr.querySelector('.btnModificar');
   btnModificar.addEventListener('click', () => {
-          actualizar2(index);
+    actualizar2(index);
   });
 };
 
 const actualizar2 = (i2) => {
-  const arrayPostLocalStorage = getTextos();
+  const arrayPostLocalStorage = data.leerLocalStorage('textos');
   arrayPostLocalStorage[i2].area = document.getElementById('newText').value;
-  if (arrayPostLocalStorage[i2].area === ''){
-   alert("Escribe su nombre");
+  if (data.editarElementosArray(arrayPostLocalStorage, i2, '')) {
+    alert('Espacio vacío');
   } else {
-  localStorage.setItem('textos', JSON.stringify(arrayPostLocalStorage));
-  pintarPost();
+    data.guardarLocalStorage('textos', arrayPostLocalStorage);
+    pintarPost(data.leerLocalStorage('textos'));
   }
 };
 
 const eliminar = (i) => {
-  const arrayPostLocalStorage = getTextos();
-  const filtrado = arrayPostLocalStorage.filter((y, posicion) => posicion !== i);
-  localStorage.setItem('textos', JSON.stringify(filtrado));   
-  pintarPost();
-  
+  const arrayPostLocalStorage = data.leerLocalStorage('textos');
+  const filtrado = data.eliminarElementoArray(arrayPostLocalStorage, i);
+  data.guardarLocalStorage('textos', filtrado);
+  pintarPost(data.leerLocalStorage('textos'));
 };
 
 btnSalir.addEventListener('click', () => {
-  cajaPost.classList.add('hide');
-  cajaMenu.classList.add('hide');
-  cajaIngreso.classList.remove('hide');
-  cajaImagen.classList.remove('hide');
+  inputPost.classList.add('hide');
+  inputMenu.classList.add('hide');
+  inputIngreso.classList.remove('hide');
+  inputImagen.classList.remove('hide');
   msjErroneoLogin.classList.add('hide');
   document.getElementById('email').value = '';
   document.getElementById('contraseña').value = '';
@@ -189,11 +184,11 @@ btnRetornar.addEventListener('click', () => {
   location.reload();
   document.getElementById('email').value = '';
   document.getElementById('contraseña').value = '';
-  cajaRegistro.classList.add('hide');
-  cajaIngreso.classList.remove('hide');
+  inputRegistro.classList.add('hide');
+  inputIngreso.classList.remove('hide');
 });
 
 enlaceRegistro.addEventListener('click', () => {
-  cajaRegistro.classList.remove('hide');
-  cajaIngreso.classList.add('hide');
+  inputRegistro.classList.remove('hide');
+  inputIngreso.classList.add('hide');
 });
